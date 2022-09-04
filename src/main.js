638d66a23c15aca896365e6274ec8cb8c0ec5a18
Craig White <craigwhiteofficial@gmail.com>
2022-09-04 06:38:19 -0400

    Add Vue Router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/tailwind.css';
 import App from './App.vue';
 import { createApp } from 'vue';
+import router from './router';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
