ac9e3c2378f61dcff9aabf58ccbd806effbe63c6
Craig White <craigwhiteofficial@gmail.com>
2022-09-10 09:54:17 -0400

    Update SearchModal to be more mobile friendly

--- src/components/SearchModal.vue
+++ src/components/SearchModal.vue
@@ -33,15 +33,15 @@
         clearTimeout(state.debounce);
         state.debounce = setTimeout(async () => {
             state.search = event.target.value;
+            state.results = [];
+            state.moreResults = false;
 
             if (!state.search) {
-                state.results = [];
-                state.moreResults = false;
                 return;
             }
 
             try {
-                const response = await fetch(`https://consumet-api.herokuapp.com/meta/anilist/advanced-search?query=${state.search}&perPage=10`, {
+                const response = await fetch(`https://consumet-api.herokuapp.com/meta/anilist/advanced-search?query=${state.search}&perPage=6`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
@@ -54,33 +54,45 @@
                 state.error = error;
             }
 
-        }, 600);
+        }, 400);
     };
 </script>
 
 <template>
-    <Modal v-if="isOpen" :is-open="isOpen" placement="top" @close="$emit('close')" class="text-white">
+    <Modal v-if="isOpen" :is-open="isOpen" placement="top" @close="$emit('close')" class="text-white text-sm sm:text-base">
         <div class="flex items-center border-b border-neutral-800">
             <input @input="debounceSearch" type="text" class="w-full p-3 bg-neutral-900 outline-0">
             <FontAwesomeIcon :icon="faSearch" size="xl" class="text-neutral-700 px-3" />
         </div>
-        <ul class="grid grid-cols-2">
-            <li
-                v-for="result in state.results"
-                :key="result.id"
-                @click="$emit('openAnime', result.id); $emit('close')"
-                class="hover:bg-neutral-800 cursor-pointer p-3 flex"
-            >
-                <img
-                    :src="result.image"
-                    :alt="`Episode ${result.number}`"
-                    class="h-40 w-24 object-cover object-center mr-4"
+        <Transition
+            enter-active-class="duration-300 ease-out"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="duration-200 ease-in"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <ul v-if="state.results.length" class="sm:grid grid-cols-2">
+                <li
+                    v-for="result in state.results"
+                    :key="result.id"
+                    @click="$emit('openAnime', result.id); $emit('close')"
+                    class="hover:bg-neutral-800 cursor-pointer p-3 flex"
                 >
-                <div class="flex flex-col">
-                    <p class="mb-1">{{ result.title.romaji }}</p>
-                    <small class="text-neutral-400">{{ result.releaseDate }} {{ result.format}}</small>
-                </div>
-            </li>
-        </ul>
+                    <img
+                        :src="result.image"
+                        :alt="`Episode ${result.number}`"
+                        class="h-40 w-24 object-cover object-center mr-4"
+                    >
+                    <div class="flex flex-col">
+                        <p class="mb-2">{{ result.title.romaji }}</p>
+                        <div class="text-neutral-400">
+                            <span class="mr-2">{{ result.releaseDate }}</span>
+                            <span class="ring-1 ring-white px-2 pb-0.5">{{ result.type }}</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </Transition>
     </Modal>
 </template>
